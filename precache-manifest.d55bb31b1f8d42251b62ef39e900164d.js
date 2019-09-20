c96e806ca6c932d2a4a1aeb99007c0f087eb1c35
mikerobards <mikerobards@icloud.com>
2019-09-19 20:27:47 -0400

    Updates

---
+++ precache-manifest.d55bb31b1f8d42251b62ef39e900164d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c7a763ef08c42a5209afdac1672852",
+    "url": "/monster-rolodex2/index.html"
+  },
+  {
+    "revision": "b61cdd432ca16c4de05d",
+    "url": "/monster-rolodex2/static/css/main.d67be7e8.chunk.css"
+  },
+  {
+    "revision": "6d9732ae4975475a2c61",
+    "url": "/monster-rolodex2/static/js/2.f536d6a4.chunk.js"
+  },
+  {
+    "revision": "b61cdd432ca16c4de05d",
+    "url": "/monster-rolodex2/static/js/main.7b7699f2.chunk.js"
+  },
+  {
+    "revision": "48e04aa1245e0d00620d",
+    "url": "/monster-rolodex2/static/js/runtime~main.22f56d63.js"
+  }
+]);
